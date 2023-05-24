f73d59820478ba55979e90b1cbe3d839c18b35bc
Niels Vanhorenbeeck <nielsvhb@hotmail.com>
2023-05-24 21:05:58 +0200

    Image slider op project pagina

---
+++ src/components/image-slider.vue
@@ -0,0 +1,41 @@
+﻿<template>
+    <div id="projects">
+        <carousel :items-to-show="2">
+            <slide v-for="image in images" :key="image">
+                <img :src="image" class="h-96" alt="project" /> 
+            </slide>
+
+            <template #addons>
+                <navigation />
+            </template>
+        </carousel>
+    </div>
+</template>
+
+<script lang="ts">
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+
+export default {
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+    props:{
+      images: Array  
+    },
+    data() {
+        return {
+            
+        };
+    },
+};
+</script>
+
+<style scoped>
+button {
+    font-weight: bold;
+}
+</style>
